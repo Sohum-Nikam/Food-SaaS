1710bca6c4b88aa986b5eb6b8af530e05dd7dc74
Sohum Nikam <sohumnikam868@gmail.com>
2026-03-05 12:18:56 +0530

    Sohum

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,10 @@
 const express =  require('express');
 
 const app = express(); 
+app.use(express.json()); // Middleware to read express json data from the request body 
+
+//middleware brings data to req.body and makes it readable and usable as express files cannot be read directly by servers
+//the data can be fetched from the frontend and sent to the backend in the form of json and this middleware will make it readable by the server
 
 //Dummy route for testing the server connection
 app.get('/', (req, res) => {
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,1 +1,20 @@
 //logic of routes
+const userModel = require('../models/user.model');
+
+//controller function here:
+async function registerUser(req, res) {
+    
+    const {fullName, email, password} = req.body;  //--> destructuring the request body to get the user details
+
+    const isUserAlreadyExists = await userModel.findOne({
+        email
+    })
+
+    if(isUserAlreadyExists) {
+         return res.status(400).json({
+            message:"User already exists",
+            
+         })
+    }
+
+}
